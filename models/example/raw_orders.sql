9546c744bee42e4197ce101e8426d20a9a8e1fb9
Alex <adecarbonnieres@eugeniaschool.com>
2026-02-04 13:02:40 +0000

    update Row_orders.sql

--- models/example/raw_orders.sql
+++ models/example/raw_orders.sql
@@ -3,8 +3,33 @@
 SELECT
     1 AS id,
     'Alexandre' AS Prenom,
-    'De Carbonnieres' AS Nom,
-    20  AS Age,
+    'De Caronnieres' AS Nom,
+    20 AS Age,
     'Eugenia School' AS Ecole
 
+UNION ALL
 
+SELECT
+    2 AS id,
+    'Alexandre' AS Prenom,
+    'Palmer' AS Nom,
+    19 AS Age,
+    'Eugenia School' AS Ecole
+
+UNION ALL
+
+SELECT
+    3 AS id,
+    'Nicolas' AS Prenom,
+    'Shahata' AS Nom,
+    19 AS Age,
+    'Eugenia School' AS Ecole
+
+UNION ALL
+
+SELECT
+    4 AS id,
+    'Jonas' AS Prenom,
+    'Lavigne' AS Nom,
+    19 AS Age,
+    'Eugenia School' AS Ecole
