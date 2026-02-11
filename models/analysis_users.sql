65d227faa5f6cb57b7fd68c4c1ecc20af66da55a
Alex <adecarbonnieres@eugeniaschool.com>
2026-02-11 16:03:51 +0000

    creating analysis

--- models/analysis_users.sql
+++ models/analysis_users.sql
@@ -1,12 +1,11 @@
 {{ config(materialized='view') }}
 
 SELECT
-    id,
     user_id,
     created_at,
     last_uptdate,
-    origin,
-    destination
 FROM {{ source('raw', 'user_searches') }}
+
+WHERE created_at >= last_uptdate
    
 
