41b62ac64b15425f61ccf99ccf53ecc6c5adafd4
Alex <adecarbonnieres@eugeniaschool.com>
2026-02-11 14:51:01 +0000

    analyse

--- models/example/staging/stg_user_searches.sql
+++ models/example/staging/stg_user_searches.sql
@@ -1,9 +1,13 @@
 {{ config(materialized='view') }}
 
 SELECT
-  id,
-  user_id,
-  created_at,
-  origin,
-  destination
+    id,
+    user_id,
+    created_at,
+    last_uptdate,
+    origin,
+    destination
 FROM {{ source('raw', 'user_searches') }}
+
+WHERE created_at <= last_uptdate
+   OR last_uptdate IS NULL
